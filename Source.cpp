a3b18bf60f47ac4222af153ccef693b5a4543cfe
VladPolyvko007 <80321694+VladPolyvko007@users.noreply.github.com>
2021-04-05 15:48:39 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+t
+
+int max();
+
+int main()
+{
+	int f = 7;
+	cout << max(&f) << endl;
+	book[]
+		mas(book)
+
+	cout << f << endl;
+
+	system("pause");
+	return 0;
+}
+int max(int *a)//int a = f;
+{
+	*(a) *= *a;
+	cout << *a << endl;
+	return 0;
+}
